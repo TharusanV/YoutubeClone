1fe29e8c0297281be270265fce3982743da59457
Tharusan Vijayakumar <tharusan.vijayakumar@gmail.com>
2022-09-27 11:33:20 +0100

    Deleted the old repo just adding it again

--- main/src/components/Videos.jsx
+++ main/src/components/Videos.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import VideoCard from './VideoCard'
 
-const Videos = (videos) => {
+const Videos = ({videos}) => {
   return (
-    <div>
-      
+    <div direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+      {videos.map((item, index) => (
+        <div key={index}>
+          {item.id.videoID && <VideoCard video={item}/>}
+        </div>
+      ))};
     </div>
   )
 }
